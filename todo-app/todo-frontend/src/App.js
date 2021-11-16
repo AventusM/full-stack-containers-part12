c99f621475425452696394e3444b0770b898a711
aventusm <8antonm@gmail.com>
2021-11-16 14:17:39 +0200

    12.15 Developing through docker-compose volumes now possible by building the dev.Dockerfile and pointing docker-compose.dev towards it

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import TodoView from './Todos/TodoView'
+import TodoView from './Todos/TodoView';
 
 function App() {
   return (
     <div className="App">
+      <h1>Added through 12.15 container volume connection</h1>
       <TodoView />
     </div>
   );
