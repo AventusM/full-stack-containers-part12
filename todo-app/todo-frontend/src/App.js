43763e693a1a552f189feba1760e85c00054e478
aventusm <8antonm@gmail.com>
2021-11-17 21:01:39 +0200

    12.17-12.19 should be working now. With permission issues, use sudo to run docker-compose. It might have problems with mongo_data / redis_data directories otherwise

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Added through 12.15 container volume connection</h1>
+      <h1>12.19 Check works!</h1>
       <TodoView />
     </div>
   );
